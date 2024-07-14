9c0c798d002ac8fe36069730577cb08eaea87097
ketna20 <ketna.kumar@gmail.com>
2024-07-14 09:20:55 -0700

    useState is added to dynamically chang the tab contents when the tab is selected.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic ] = useState('components');
+
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
   }
 
   return (
@@ -36,6 +42,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          <div id="tab-content"> 
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code} </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
